55681f87ff2b4d11f83f99941c4ca9b5dfd30699
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2021-06-10 22:40:29 +0530

    starting restructuring

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ImageCarousel from "./components/ImageCarousel";
 import "bootstrap/dist/css/bootstrap.css";
 import * as faceapi from "face-api.js";
 import FaceCarousel from "./components/FaceCarousel";
-import { withWidth } from "@material-ui/core";
 import WebcamComponent from "./components/webcamComponent";
 function App() {
   const [modelLoaded, setModelLoaded] = useState(false);
@@ -23,17 +22,10 @@ function App() {
     results: { label: "Results", status: 0 },
   });
   useEffect(() => {
-    console.log("rerender");
-  }, []);
-  useEffect(() => {
-    // console.log(faceapi);
-    //faceapi.TinyFaceDetector();
-    // faceapi.ssdMobilenetv1();
     async function loadModel() {
       const MODEL_URL = process.env.PUBLIC_URL + "/models";
       await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
       setModelLoaded(true);
-      // console.log(faceapi);
     }
     loadModel();
   });
@@ -45,10 +37,6 @@ function App() {
   }
   async function imageFromWebcam(addedImage) {
     let aspect_ratio = 16 / 9;
-    /**
-     * takes user added image and extracts faces
-     */
-    //let addedImage = URL.createObjectURL(e.target.files[0]);
     console.log(addedImage);
     document.getElementById("hiddenImage").src = addedImage;
     var options = new faceapi.SsdMobilenetv1Options({
@@ -58,8 +46,6 @@ function App() {
     let predictions = await faceapi.detectAllFaces("hiddenImage", options);
     let temp = [];
     for (var i = 0; i < predictions.length; i++) {
-      let x = predictions[i].box._x;
-      let y = predictions[i].box._y;
       let width = predictions[i].box._width;
       let height = predictions[i].box.height;
       let adjustmentx = 0;
@@ -78,33 +64,16 @@ function App() {
         ),
       ]);
       predictions[i].url = canv[0].toDataURL();
-      // console.log(canv[0]);
-      //let tempUrl = canv[0].toDataURL();
-      //canv[0].drawImage(tempUrl, 0, 0, 36, 64);
       temp.push(canv[0].toDataURL());
-      // canv = null;
     }
+
     let f = [...faces, ...temp];
     console.log(f);
-    //f.push(...predictions);
     setFaces(f);
-
-    // let temp = [...uploadedImages];
-    // console.log(e);
-    // temp.push({
-    //   url: URL.createObjectURL(e.target.files[0]),
-    //   processed: false,
-    // });
-    // setUploadedImages(temp);
   }
   async function addImage(addedImage) {
     let aspect_ratio = 16 / 9;
-    /**
-     * takes user added image and extracts faces
-     */
-    //let addedImage = URL.createObjectURL(e.target.files[0]);
     document.getElementById("hiddenImage").src = addedImage;
-
     var options = new faceapi.SsdMobilenetv1Options({
       minConfidence: 0.4,
     });
@@ -112,8 +81,6 @@ function App() {
     let predictions = await faceapi.detectAllFaces("hiddenImage", options);
     let temp = [];
     for (var i = 0; i < predictions.length; i++) {
-      let x = predictions[i].box._x;
-      let y = predictions[i].box._y;
       let width = predictions[i].box._width;
       let height = predictions[i].box.height;
       let adjustmentx = 0;
@@ -132,25 +99,39 @@ function App() {
         ),
       ]);
       predictions[i].url = canv[0].toDataURL();
-      // console.log(canv[0]);
-      //let tempUrl = canv[0].toDataURL();
-      //canv[0].drawImage(tempUrl, 0, 0, 36, 64);
       temp.push(canv[0].toDataURL());
-      // canv = null;
     }
     let f = [...faces, ...temp];
     console.log(f);
-    //f.push(...predictions);
     setFaces(f);
-
-    // let temp = [...uploadedImages];
-    // console.log(e);
-    // temp.push({
-    //   url: URL.createObjectURL(e.target.files[0]),
-    //   processed: false,
-    // });
-    // setUploadedImages(temp);
   }
+
+  /**
+   * Functions
+   *
+   * handleFileInput
+   * handles input from file
+   * sends cropped faces to database or reference array
+   *
+   * handleWebcamInput
+   * handles input from webcam
+   * sends cropped faces to databse or reference array
+   *
+   * addFacesToDatabase
+   * adds faces to database
+   *
+   * addFacesToReference
+   * adds faces to reference
+   *
+   * removeFaceFromDatabse
+   * input: index (number)
+   * removes face from database
+   *
+   * removeFaceFromReference
+   * input: index (number)
+   * removes face from reference
+   */
+
   const removeFace = (index) => {
     var temp = [];
     for (var i = 0; i < faces.length; i++) {
